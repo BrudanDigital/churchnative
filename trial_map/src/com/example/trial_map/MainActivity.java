45e76115a2fdcb9d82da2cb9f0e102437946222d
nsubugak <nkasozi@gmail.com>
2013-06-24 13:11:15 +0300

    my first commit

---
+++ trial_map/src/com/example/trial_map/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.trial_map;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+	
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState)
+        {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);     
+    }
+
+
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+    
+}
